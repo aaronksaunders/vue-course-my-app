1e0c2d8db91160623a5242e73b5ec05e412c7c52
Aaron Saunders <aaron@clearlyinnovative.com>
2019-08-24 13:03:40 -0400

    initial checkin

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MyName name="Aaron" @was-clicked="handleWasClicked"></MyName>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import MyName from "./components/MyName.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    MyName
+  },
+  methods: {
+    handleWasClicked: function(_data) {
+      debugger;
+      alert(_data);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
