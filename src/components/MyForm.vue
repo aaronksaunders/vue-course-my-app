201068721ba8ecf92e5fe71dbeede9a14fafabc2
aaron k saunders <aaronksaunders@gmail.com>
2019-08-24 18:54:25 -0400

    add comments to the code

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -12,17 +12,22 @@
 export default {
   name: "MyForm",
   props: {
+    // this is the data being passed into the component
     initialFormData: Object
   },
   data() {
     return {
+      // here we are using the initialFormData to set the v-model data
+      // that will be used in the component
       userData : this.initialFormData.name || {}
     }
   },
   methods: {
     /**
-     * this will emit an event to the parent with the value from the
-     * userData property
+     * this will emit an "form-clicked" event to the parent with the
+     * value from the userData property
+     * 
+     * the userData will only contain the firstName and LastName
      */
     handleBtnClicked: function() {
       this.$emit("form-clicked", this.userData);
--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -2,8 +2,10 @@
   <div class="list-container">
     <h3>Show List Of Users</h3>
     <ol>
-      <li v-for="e in this.incomingListData" :key="e.id">
-        <span @click="$emit('list-clicked',e)">{{ e.name.firstName }} {{ e.name.lastName }}</span>
+      <li v-for="listItem in this.incomingListData" :key="listItem.id">
+        <span
+          @click="handleItemClicked(listItem)"
+        >{{ listItem.name.firstName }} {{ listItem.name.lastName }}</span>
       </li>
     </ol>
   </div>
@@ -12,9 +14,19 @@
 export default {
   name: "MyList",
   props: {
+    // this is the list data to render
     incomingListData: Array
   },
-  methods: {}
+  methods: {
+    /**
+     * this will emit an "list-clicked" event to the parent with the
+     * value from the current list item
+     *
+     */
+    handleItemClicked: function(_listItem) {
+      this.$emit("list-clicked", _listItem);
+    }
+  }
 };
 </script>
 
@@ -22,6 +34,6 @@ export default {
 .list-container {
   background-color: aliceblue;
   padding: 10px;
-  margin : 10px;
+  margin: 10px;
 }
 </style>
