fa07a9333e1ab0df028077b46f417a3f2a3a8c96
Aaron Saunders <aaron@clearlyinnovative.com>
2019-08-24 13:04:36 -0400

    all changes

---
+++ src/components/MyName.vue
@@ -0,0 +1,16 @@
+<template>
+  <div @click="gotClicked">{{name}}</div>
+</template>
+<script>
+export default {
+  name: "MyName",
+  props: {
+    name: String
+  },
+  methods: {
+    gotClicked: function() {
+      this.$emit("was-clicked", this.name + " - " + new Date());
+    }
+  }
+};
+</script>
