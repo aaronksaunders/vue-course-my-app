3f4f384cf6998c75e4907d8d01fd812090cbfe6c
Aaron Saunders <aaron@clearlyinnovative.com>
2019-08-24 15:27:58 -0400

    added basic list

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,37 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <MyName name="Aaron" @was-clicked="handleWasClicked"></MyName>
+    <my-list :incomingListData="anArray" @list-clicked="handleListClicked"></my-list>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import MyName from "./components/MyName.vue";
+import MyList from "./components/MyList.vue";
 
 export default {
   name: "app",
   components: {
     HelloWorld,
-    MyName
+    MyName,
+    MyList
+  },
+  data() {
+    return {
+      anArray: [
+        { id: 0, name: { firstName: "Andrea", lastName: "Saunders" } },
+        { id: 1, name: { firstName: "Aaron", lastName: "Saunders" } }
+      ]
+    };
   },
   methods: {
     handleWasClicked: function(_data) {
       debugger;
       alert(_data);
+    },
+    handleListClicked: function(_data) {
+      alert(_data.name.firstName);
     }
   }
 };
---
+++ src/components/MyList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Show List Of Users</h3>
+    <ol>
+      <li v-for="e in this.incomingListData">
+        <span @click="$emit('list-clicked',e)">{{ e.name.firstName }} {{ e.name.lastName }}</span>
+      </li>
+    </ol>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyList",
+  props: {
+    incomingListData: Array
+  },
+  methods: {}
+};
+</script>
